03d347fef91a7a36ea4c2bfb410c8e363158776a
coco <coco030.030@gmail.com>
2025-07-08 20:43:37 +0900

    Add files via upload
    기본 파일 작업.

---
+++ PreWatch/src/main/java/com/springmvc/repository/MemberRepositoryImpl.java
@@ -0,0 +1,15 @@
+package com.springmvc.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.springmvc.domain.Member;
+@Repository
+public class MemberRepositoryImpl implements MemberRepository {
+
+	@Override
+	public void save(Member member) {
+		
+		
+	}
+
+}
