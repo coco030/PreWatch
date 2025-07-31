b7704eff4a677ea76e0588f8624a13f32c675d92
coco030 <coco030030@gmail.com>
2025-07-31 09:35:15 +0900

    js에서 최신문법을 못 읽어내서 빨간줄 뜨길래 옛날 방식으로 바꿈.

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -1,4 +1,5 @@
-const isLoggedIn = document.getElementById("isLoggedIn")?.value === "true";
+var elem = document.getElementById("isLoggedIn");
+var isLoggedIn = elem && elem.value === "true";
 if (!isLoggedIn) {
     alert("로그인 후 이용 가능합니다.");
     return;
